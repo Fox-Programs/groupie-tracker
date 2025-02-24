fa211b82b0a4189337973600626fbb2e53699765
falexis <alexis.facon@ynov.com>
2025-02-24 10:14:29 +0100

    change

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"groupie-tracker/models"
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -19,14 +20,20 @@ func main() {
 		fmt.Println("Erreur :", err)
 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { // Page principal avec le premier "/"
-		tpl := template.Must(template.ParseFiles("../templates/index.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		tpl := template.Must(template.ParseFiles("templates/index.html"))
 		tpl.Execute(w, data)
 	})
+
 	http.HandleFunc("/artiste", func(w http.ResponseWriter, r *http.Request) { // Page secondaire acessible depuis la première page
-		tpl := template.Must(template.ParseFiles("../templates/artiste.html"))
+		tpl := template.Must(template.ParseFiles("templates/artiste.html"))
 		tpl.Execute(w, nil)
 	})
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	log.Println("Serveur démarré sur http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
 
 	fmt.Print("Pour accéder au serveur lancé, aller sur la page http://localhost:8080") // Lancement du serveur sur le port 8080
 	http.ListenAndServe(port, nil)                                                      // Utilisée pour démarrer un serveur HTTP (nil car on va utiliser nos propres fonctions)
